e73ebad1e3b834cdd58fbce78b9fd098fd13a2e5
SBhome <open1234104@gmail.com>
2016-07-05 03:08:44 +0900

    Scene 수정

---
+++ PlanAndPlay/Assets/02.Scripts/csChapter/csChapterButtonClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csChapterButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PressBack(){
+		Application.LoadLevel("Lobby");
+	}
+
+	public void PressChapter1(){
+		Application.LoadLevel("Stage");
+	}
+}
